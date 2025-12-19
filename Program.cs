e23ce27051154464de542d4c9e991b6c61adaa84
Oleg569362836 <dzagoev4072@gmail.com>
2025-12-19 10:48:03 +0300

    Проект по продажам в супермаркете

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Уч.практика_2.Учет_продаж_в_супермаркете
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
